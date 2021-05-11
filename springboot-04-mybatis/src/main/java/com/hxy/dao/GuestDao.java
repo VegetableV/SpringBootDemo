2ffe1be44d788a01ea1750ae7ed44736881d6d49
${VegetableV} <${hxy1377883453@163.com}>
2021-05-11 11:16:07 +0800

    第十五次提交，用于简化十四次的配置文件

--- springboot-04-mybatis/src/main/java/com/hxy/dao/GuestDao.java
+++ springboot-04-mybatis/src/main/java/com/hxy/dao/GuestDao.java
@@ -2,6 +2,7 @@ package com.hxy.dao;
 
 import com.hxy.domain.Guest;
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Select;
 import org.springframework.stereotype.Repository;
 
 import java.util.ArrayList;
@@ -9,6 +10,6 @@ import java.util.List;
 
 @Mapper
 public interface GuestDao {
-
+    @Select("select * from guest")
     List<Guest> reList();
 }
